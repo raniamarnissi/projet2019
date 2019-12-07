4dfe3d082f98ae9978fbc5effb8c2fcecae5f706
Emna Hosni <hosniemna99@gmail.com>
2019-12-07 10:32:28 +0100

    afficher les paniers dee la base dans un tableau

--- chariot.php
+++ chariot.php
@@ -6,23 +6,31 @@
 
     if (!empty($_POST)){
     //Récupérer les valeurs saisies par l'utilisateur
-            
+
     $cnom= $_POST['cid'];
     $pnom = $_POST['pid'] ;
     $qte= $_POST['qte'] ;
     $statut= $_POST['statut'] ;
 
     $listpanier = $chariot->addNewpanier($cnom,$pnom,$qte,$statut);
-
-    $listpanier = $chariot->readAllpanier();
-    while($data = $listpanier->fetch()){
+    echo '<table border="1">';
         
-        echo $data['cid'];
-        echo $data['pid'];
-        echo $data['qte'];
-        echo $data['statut'];
-    }
-}
+        $listpanier = $chariot->readAllpanier();
+        while($data = $listpanier->fetch()){
+            echo '<tr>';
+            echo '<td>'.$data['cid'].'</td>';
+            echo '<td>'.$data['pid'].'</td>';
+            echo '<td>'.$data['qte'].'</td>';
+            echo '<td>'.$data['statut'].'</td>';
+            echo '</tr>';
+            
+        }
+        
+        }
+
+    echo '</table>' ;
+    
+    
     header("Location:panier.phtml");
 
 ?>
