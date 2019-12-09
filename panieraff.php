9619e6fd6301f3c4d76fdc40a2fac6b73882d06f
Emna Hosni <hosniemna99@gmail.com>
2019-12-09 16:22:31 +0100

    reucuper prix unitaire fonctionne

--- panieraff.php
+++ panieraff.php
@@ -13,10 +13,18 @@ include 'panier.class.php';
 
 if (isset($_SESSION)) {
     $_SESSION['nomproduit']=$_GET['nomp'];
-    $_SESSION['idproduit']=$_GET['idp']; 
-    $_SESSION['$prixunitaire']=$prixunitaire;
+    $_SESSION['idproduit']=$_GET['idp'];   
+}
+if (isset($_GET['nomp'])) {
+        $p=$_GET['idp'];
+        $pro = new PANIER;
+        $res = $pro->getprix($p);
+        $data = $res->fetch();
+        $_prixunitaire = $data['prix'];
+        $_SESSION['prix_uni'] =$_prixunitaire;
 }
 
+//var_dump($_SESSION);
 ?>
     <div class="container">
         <form action="chariot.php" method="post">
@@ -37,19 +45,17 @@ if (isset($_SESSION)) {
                             <option value="5">5</option>
                     </select>
                 prix unitaire
-                <input type="text" class="form-control" disabled value="$prixunitaire">
-                prix total
-                    <div class="alert alert-success alert-dismissable">
-                        <input type="text" class="form-control" disabled value="<?=$_prixtotal?>">
-                    </div>
+                <input name= "prixu" type="text" class="form-control" disabled value="<?=$_prixunitaire?>">
+ 
                 statut delevry</br> 
                 <select name="statut" id="statut" class="form-control">
                         <option value="1">livrer</option>
                         <option value="0">non livrer</option>
                 </select>
-                <button type="submit" name="" class="btn btn-outline-success">confirm order</button> 
+                <button type="submit" name="" class="btn btn-outline-success">enregistrer dans votre panier</button> 
                 </br>
-                <button type="reset" class="btn btn-outline-danger" ><a href="">annuler</a></button> </br>
+                <button type="reset" class="btn btn-outline-danger">annuler</button>
+
             </div>
         </form>
     </div>
